32ca5c826e8e84909b737a5d55110db807093da2
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-08-29 18:44:46 +0200

    Fixed comments for JSX

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = (() => {
   return (
     <header className="header container">
-    // Font Awesome icon that shows or hides the list of locations and search feature
-    // Is also available for keyboard users
+    {/* Font Awesome icon that shows or hides the list of locations and search feature
+      Is also available for keyboard users */}
       <i aria-label="The main menu button" tabindex={0} className="fa fa-bars" aria-hidden="true" onKeyDown={(event) => {if((event.which === 13)||(event.which === 32)){document.getElementById('sidebar').classList.toggle('hide-sidebar')}}} onClick={() => {document.getElementById('sidebar').classList.toggle('hide-sidebar')}}></i>
       <h1>Grasse Surroundings</h1>
     </header>
--- src/LocationInformation.js
+++ src/LocationInformation.js
@@ -1,19 +1,21 @@
 import React from 'react';
 //Displays the location data. Updated by the App parent Component
+
+//It is normally hidden under the screen
+//When toogled, it emerges a bit from the bottom of the page, inviting the
+// user to click for more info about the location
+
+// For keyboard users, toggles the appropriate function when pressing enter
+// and switches the focus straight to the location window when pressing
+//tab
 const LocationInformation = ((props) => {
     return(
-      //It is normally hidden under the screen
-      //When toogled, it emerges a bit from the bottom of the page, inviting the
-      // user to click for more info about the location
       <aside id="location-information" className="location-information hide">
         <button id ='show-info'
           aria-label='button to display information about the selected location'
           onClick={()=>{
             document.getElementById("location-information").classList.toggle('slide-in');
           }}
-          // For keyboard users, toggles the appropriate function when pressing enter
-          // and switches the focus straight to the location window when pressing
-          //tab
           onKeyDown={(event) => {
             if((event.which === 13)){document.getElementById("location-information").classList.toggle('slide-in')}
             if((event.which === 9)){document.getElementById("back").focus()}
