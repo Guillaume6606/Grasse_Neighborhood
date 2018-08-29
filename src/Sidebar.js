d6c252295ec0d2184e6fc3a3ef06270d3f9213f6
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-08-29 18:21:46 +0200

    Made Sidebar a functional Component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const Sidebar = (() => {
+const Sidebar = ((props) => {
     return (
       <nav id='sidebar' className="sidebar hide-sidebar">
-        <input type="text" placeholder="Filter Locations" onChange={event => this.props.filterLocations(event.target.value)}/>
+        <input type="text" placeholder="Filter Locations" onChange={event => props.filterLocations(event.target.value)}/>
         <ul>
-         {this.props.locations.map(location => (
-          <li className="sidebar-location" key={location.title}>
-            <a tabindex={0} aria-label={`Link to access information about ${location.title}`} onClick={() => this.props.clickLocation(location.location)}
+         {props.locations.map(location => (
+          <li onClick={() => props.clickLocation(location.location)} className="sidebar-location" key={location.title}>
+            <a tabindex={0} aria-label={`Link to access information about ${location.title}`}
                onKeyDown={(event) => {
                  if((event.which === 13)||(event.which === 32)){
-                   this.props.clickLocation(location.location);
+                   props.clickLocation(location.location);
                    document.getElementById('show-info').focus();
                  }}}
               >{location.title}</a>
           </li>
         ))}
         </ul>
-        <button aria-label="button to reset the search of locations" className='reset-button'onClick={()=>(this.props.resetLocations())}>Reset Search</button>
+        <button aria-label="button to reset the search of locations" className='reset-button'onClick={()=>(props.resetLocations())}>Reset Search</button>
       </nav>
     )
 })
