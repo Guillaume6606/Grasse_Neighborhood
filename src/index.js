9ae693e36888771ca3b1c01e0580f0a3b10e6662
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-08-29 19:31:58 +0200

    deleted useless service worker

--- src/index.js
+++ src/index.js
@@ -4,18 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-    .then(() => {
-      console.log('serviceWorker registered')
-    })
-    .catch((err) => {
-      console.log(`There was an error with the serviceWorker: ${err}`);
-
-    });
-  });
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
